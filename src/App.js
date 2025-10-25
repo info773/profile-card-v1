d44c50beb6fc9ee537d48d6fac8d9b7ca129c8b5
Berry <info@felix-behrens.de>
2025-10-25 10:28:13 +0200

    Solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
     </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="/me.JPG" alt="Not actually me"></img>
+      <p className="small">Is it tho?</p>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Felix Behrens</h1>
+      <p>
+        Hobbyist trying to create some usefull stuff while trying to have fun.
+        Also I need more text so it looks similar to the example of Jonas. May
+        just another sentence, then we'll be fine.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill name="React" emoji="❤️" style={{ backgroundColor: "red" }} />
+      <Skill name="Html" emoji="🏋🏻‍♀️" style={{ backgroundColor: "blue" }} />
+      <Skill name="CSS" emoji="🥑" style={{ backgroundColor: "yellow" }} />
+      <Skill
+        name="Javascript"
+        emoji="🧠"
+        style={{ backgroundColor: "green" }}
+      />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <span style={props.style} className="skill">
+      {props.name} {props.emoji}
+    </span>
   );
 }
 
